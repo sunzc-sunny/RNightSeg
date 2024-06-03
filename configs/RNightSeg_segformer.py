4935bf2ed96c5f983d9e6dd7b237557e9e735003
sunzhichao <sunzc@hust.edu.cn>
2024-06-03 22:26:04 +0800

    1

--- configs/RNightSeg_segformer.py
+++ configs/RNightSeg_segformer.py
@@ -1,7 +1,8 @@
 _base_ = [
     '../../_base_/models/segformer_mit-b0.py',
     '../../_base_/datasets/nightcity_1024x2048.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_80k.py'
+    '../../_base_/default_runtime.py',
+    '../../_base_/schedules/schedule_80k.py'
 ]
 
 checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b5_20220624-658746d9.pth'  # noqa
@@ -13,13 +14,13 @@ model = dict(
         embed_dims=64,
         num_layers=[3, 6, 40, 3]),
     decode_head=dict(
-        type='SegformerParallelHeadNew',
+        type='SegformerParallelHead',
         in_channels=[64, 128, 320, 512],
         loss_decode=
         [dict(type='ReflectanceLossV3', loss_weight=2.0),
-         dict(type='L_TV', loss_weight=0.1),
+         dict(type='L_TV', loss_weight=0.05),
          dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-         dict(type='ColorProcessingLossV2', loss_weight=0.1)]
+         dict(type='ColorProcessingLossV2', loss_weight=0.01)]
 ))
 
 
